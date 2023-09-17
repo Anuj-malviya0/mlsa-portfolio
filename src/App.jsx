ae5ef3f47f9dea56c9f0a043af1dcda48df29063
Anuj Malviya <75200824+Anuj-malviya0@users.noreply.github.com>
2023-09-17 08:36:38 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Anuj Malviya",
+  title: "Developer & Automator",
+  email: "anujmalviya850@gmail.com",
+  gitHub: "https://github.com/Anuj-malviya0",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/anuj-malviya-a531b4204/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/Anujmalviya_",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
